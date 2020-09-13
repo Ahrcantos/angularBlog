029955bae2a38e49f5aa3f74a5ce11c55aa124de
Jonathan Scheil <jonathan_scheil@web.de>
2020-09-13 21:21:50 +0200

    created new artikel componen please enter the commit message for your changes. Lines starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtikelComponent } from './artikel/artikel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtikelComponent
   ],
   imports: [
     BrowserModule,
